c0a8a0049ad1fcbd8952d50570fac3c182d35c3c
farcrew-94 <whitegeng94@gmail.com>
2017-01-06 15:36:12 +0800

    add association between user and articles and add debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    debugger
     @article = Article.new(article_params) #article params, use for submitting the right value and it's a method !
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   # The validations
   #The attribute is needed and cannot be null and the length must be consider as follow
   validates :title, presence:true, length: {minimum: 3, maximum: 50}
   validates :description, presence:true, length: {minimum: 10, maximum: 300}
+  validates :user_id, presence: true
 
 end
