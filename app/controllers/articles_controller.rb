1919b621b988b16794267f9c308ae1b127f3ae82
farcrew-94 <whitegeng94@gmail.com>
2017-01-08 23:32:59 +0800

    Signup User from UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    
     @article = Article.new(article_params) #article params, use for submitting the right value and it's a method !
     @article.user = User.first
     if @article.save
