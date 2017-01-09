2a301753921da80d3474877908d0131eeeb99143
farcrew-94 <whitegeng94@gmail.com>
2017-01-10 04:25:01 +0800

    Add admin user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -38,6 +39,14 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5) #Make another instacne varibale to make easy for paginate and sending parameter
   end
 
+  #destroy action that Admin only can make
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted!"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password) #it use user model to do the action such as validation..refer back at user model!
@@ -48,8 +57,15 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "Forbidden!! You can only edit your own account!"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Forbidden!! Only admin users can perform that action!"
       redirect_to root_path
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User <ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy #everytime a user is destroy, all the article created by the user also be destroyed
   before_save {self.email = email.downcase}
   validates :username, presence: true,
             uniqueness: { case_sensitive: false},
